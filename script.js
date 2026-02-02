523a21d9f3118e5dbb3dac4526e39bd45490e6d5
Kolobok010 <165009430+Kolobok010@users.noreply.github.com>
2026-02-02 21:34:33 +0200

    Add form validation for submission

--- script.js
+++ script.js
@@ -36,3 +36,33 @@ removeBtn.addEventListener("click", function () {
         newElement = null;
     }
 });
+
+const form = document.getElementById("myForm");
+const formName = document.getElementById("formName");
+const email = document.getElementById("email");
+const errorMsg = document.getElementById("error");
+
+form.addEventListener("submit", function (event) {
+
+    let nameValue = formName.value.trim();
+    let emailValue = email.value.trim();
+
+    errorMsg.textContent = "";
+
+    if (nameValue === "" || emailValue === "") {
+        event.preventDefault();
+        errorMsg.textContent = "Visi lauki jāaizpilda!";
+    }
+    else if (!emailValue.includes("@") || !emailValue.includes(".")) {
+        event.preventDefault();
+        errorMsg.textContent = "Nepareizs e-pasta formāts!";
+    }
+    else if (nameValue.length < 2) {
+        event.preventDefault();
+        errorMsg.textContent = "Vārdam jābūt vismaz 2 simboliem!";
+    }
+    else {
+        alert("Forma veiksmīgi iesniegta!");
+    }
+});
+
